797472f9396358399a06638b278ee2eb4acae846
Andres Arevalo <andresfelipear@gmail.com>
2021-11-26 13:17:37 -0800

    adding string or number array ts annotation

--- ts-basic.ts
+++ ts-basic.ts
@@ -74,9 +74,9 @@ const exerciseOne = () => {
     // • Add type annotations (as explicit as possible)
     // • Fix errors (if applicable)
   
-    const sequence= Array.from(Array(10).keys());
+    const sequence:number[]= Array.from(Array(10).keys());
     const animals:string[] = ['pangolin', 'aardvark', 'echidna', 'binturong'];
-    const stringsAndNumbers:any[] = [1, 'one', 2, 'two', 3, 'three'];
+    const stringsAndNumbers:(string|number)[] = [1, 'one', 2, 'two', 3, 'three'];
     const allMyArrays = [sequence, animals, stringsAndNumbers];
   
     console.log('Exercise 1.5', allMyArrays);
